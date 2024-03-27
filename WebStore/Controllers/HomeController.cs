8a98235d3b148be4d3d7c29c1852b05026dfcc29
Usermaster001 <toworkallmylife@gmail.com>
2024-03-27 20:22:29 +0300

    Страничка со списком сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,18 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
-namespace WebStore.Controllers
+namespace WebStore.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private static readonly List<Employee> __Empoyees = new()
     {
-        public IActionResult Index()
-        {
-            //return Content("Данные из первого контроллера");
-            return View();
-        }
+        new Employee { Id = 1, LastName = "Иванов", Name = "Иван", Patronymic = "Иванович", Age = 23},
+        new Employee { Id = 2, LastName = "Петров", Name = "Петр", Patronymic = "Петрович", Age = 27},
+        new Employee { Id = 3, LastName = "Сидоров", Name = "Сидр", Patronymic = "Сидрович", Age = 33}
+    };
+    public IActionResult Index()
+    {
+        //return Content("Данные из первого контроллера");
+        return View();
+    }
 
-        public string ConfiguredAction(string value)
-        {
-            return $"Hello {value}";
-        }
+    public string ConfiguredAction(string value)
+    {
+        return $"Hello {value}";
+    }
+
+    public IActionResult Employees()
+    {
+        return View(__Empoyees);
     }
 }
+
