1cf59dfe4d7beac84d16f218e5949f3d9db616de
Usermaster001 <toworkallmylife@gmail.com>
2024-03-27 18:22:20 +0300

    Добавление контролера и не рабочий program.cs

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,6 +1,16 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var services = builder.Services;
+services.AddControllersWithViews();    //????????
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
 //Добавление файла конфигурации
 //builder.Configuration.AddJsonFile("anotherconfig.json", true, true);
 //builder.Configuration.AddCommandLine(args);
@@ -9,6 +19,17 @@ var app = builder.Build();
 //var configuration = app.Configuration;
 //var greetings = configuration["CustomGreetings"];
 
-app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+
+//app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+
+app.MapGet("/throw", () =>
+{
+    throw new ApplicationException("Ошибка в программе!");
+});
+
+//app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "defult",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
