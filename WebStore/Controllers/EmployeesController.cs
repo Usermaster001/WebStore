7ca74fd39db2b71c13aaf3b02d27753ae1985298
Usermaster001 <toworkallmylife@gmail.com>
2024-03-28 12:52:33 +0300

    Котроллер сотрудников

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> __Empoyees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", Name = "Иван", Patronymic = "Иванович", Age = 23},
+            new Employee { Id = 2, LastName = "Петров", Name = "Петр", Patronymic = "Петрович", Age = 27},
+            new Employee { Id = 3, LastName = "Сидоров", Name = "Сидр", Patronymic = "Сидрович", Age = 33}
+        };
+
+        public IActionResult Index()
+        {
+            var result = __Empoyees;
+            return View(result);
+        }
+    }
+}
